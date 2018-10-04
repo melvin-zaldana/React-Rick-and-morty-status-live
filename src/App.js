cd102978e2bfda101ac6dd0a4c9591cf13a3bd90
melvin-zaldana <melvin.zaldana91@gmail.com>
2018-10-03 23:54:31 -0500

    First commit - react app

--- src/App.js
+++ src/App.js
@@ -3,26 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    characters : []
+  }
+  componentWillMount(){
+          fetch('https://rickandmortyapi.com/api/character/')
+          .then(res => res.json())
+          .then( character => {
+            this.setState({
+              characters: character.results
+            })
+          })
+        }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+      <div className="row mt-4">
+        <div className="col-md-12">
+        <h1 class="display-3">Rick and Morty - All Characters Status Live</h1>
+        </div>
+      </div>
+          <div className="row mt-4">
+            
+        {this.state.characters.map(
+          character => <div className="col-md-3">
+          <div className="card mt-4">
+            <img className="card-img-top" src={character.image} alt="Card image cap"/>
+            <div className="card-header">
+              <h5 classname="card-title">{character.name}</h5>
+              <span className="badge badge-pill badge-dark ml-2">{character.status}</span>
+            </div>
+            <div className="card-body">
+              <h6 className="card-subtitle mb-2 text-muted">{character.species}</h6>
+              <p className="card-text">{character.gender}</p>
+            </div>
+          </div>
+          </div>
+
+          )}
+
+          </div>
+         </div>
       </div>
     );
   }
 }
 
 export default App;
+
+//
+// const
+// arrow function
+// lexical this
+// rest 
+// spread operator
+// template
